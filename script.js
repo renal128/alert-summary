81203b59b79c17c45cc30c123b3b19158a0cbece
renal128 <renal128@gmail.com>
2022-11-08 21:12:41 -0800

    Add URL and time of last alert

--- script.js
+++ script.js
@@ -20,6 +20,16 @@ const getRowBrightness = function (numAlerts) {
     }
 }
 
+const dateFormatOptions = { 
+    weekday: 'short', 
+    month: 'numeric', 
+    day: 'numeric', 
+    hour: 'numeric', 
+    minute: 'numeric',
+    timeZone: 'PST',
+    timeZoneName: 'short'
+};
+
 const buildReportButton = document.getElementById("buildReportButton");
 const copyButton = document.getElementById("copyButton");
 const rawInputField = document.getElementById("rawInput");
@@ -30,6 +40,7 @@ tableHeader.appendChild(createCell("th", "Alert Name"));
 tableHeader.appendChild(createCell("th", "Total"));
 tableHeader.appendChild(createCell("th", "Triggered"));
 tableHeader.appendChild(createCell("th", "Warn"));
+tableHeader.appendChild(createCell("th", "Last Date (PT)"));
 tableHeader.appendChild(createCell("th", "Status"));
 tableHeader.appendChild(createCell("th", "Notes"));
 
@@ -37,18 +48,19 @@ buildReportButton.onclick = function () {
     console.log("hello");
     const rawInput = rawInputField.value;
     const lines = rawInput.split("\n");
-    const regex = /[0-9]:[0-9][0-9]/g;
+    const timeRegex = /[0-9]:[0-9][0-9]/g;
+    const ddUrlRegex = /https:\/\/app\.datadoghq\.com\/monitors\/\d+\?.+&to_ts=(\d+).*/;
 
     let alerts = {};
+    let alertText = null;
 
     for (let i = 1; i < lines.length; i++) {
         const line = lines[i];
         const prevLine = lines[i - 1];
-        const prevLineHasTime = !!prevLine.match(regex);
+        const prevLineHasTime = !!prevLine.match(timeRegex);
         if (prevLineHasTime) {
             let triggered = 0;
             let warn = 0;
-            let alertText = '';
             let status = ''
             if (line.startsWith("Triggered: ")) {
                 alertText = line.substring(11);
@@ -61,16 +73,28 @@ buildReportButton.onclick = function () {
             } else if (line.startsWith("Recovered: ")) {
                 alertText = line.substring(11);
                 status = 'recovered';
-            } 
+            }
 
             if (status) {
                 if (!(alertText in alerts)) {
-                    alerts[alertText] = { "triggered": 0, "warn": 0, "last_status": "" };
+                    alerts[alertText] = { 
+                        "text": alertText, 
+                        "triggered": 0, 
+                        "warn": 0, 
+                        "last_status": "", 
+                        "last_ts": null
+                    };
                 }
                 alerts[alertText]["triggered"] += triggered;
                 alerts[alertText]["warn"] += warn;
                 alerts[alertText]["last_status"] = status;
+                alerts[alertText]["prev_ts"] = alerts[alertText]["last_ts"];
+                alerts[alertText]["last_ts"] = null;
             }
+        }
+        if (alertText != null && alertText in alerts && line.match(ddUrlRegex)) {
+            alerts[alertText]["url"] = line;
+            alerts[alertText]["last_ts"] = line.match(ddUrlRegex)[1];
         }
     }
     console.log(alerts);
@@ -78,37 +102,61 @@ buildReportButton.onclick = function () {
     let newRowsData = [];
     let sumTriggered = 0;
     let sumWarn = 0;
-    for (const [alertText, data] of Object.entries(alerts)) {
-        const t = data["triggered"];
-        const w = data["warn"];
-        const s = data["last_status"];
-        newRowsData.push([alertText, t + w, t, w, s]);
-        sumWarn += w;
-        sumTriggered += t;
+    let alertsArray = [];
+    for (const [alertText, alertData] of Object.entries(alerts)) {
+        alertsArray.push(alertData)
+        sumWarn += alertData["warn"];
+        sumTriggered += alertData["triggered"];
     }
     let sumTotal = sumWarn + sumTriggered;
 
-    newRowsData.sort((a, b) => {
-        if (a[1] != b[1])
-            return b[1] - a[1];
-        if (a[0] > b[0])
+    alertsArray.sort((a, b) => {
+        const aTotal = a["warn"] + a["triggered"]
+        const bTotal = b["warn"] + b["triggered"]
+        if (aTotal != bTotal)
+            return bTotal - aTotal;
+        if (a["triggered"] > b["triggered"])
             return 1
         return 0
     });
 
     let newRows = [tableHeader];
-    for (var i = 0; i < newRowsData.length; i++) {
-        const alertName = newRowsData[i][0];
-        const total = newRowsData[i][1];
-        const triggered = newRowsData[i][2];
-        const warn = newRowsData[i][3];
-        const status = newRowsData[i][4];
+    for (var i = 0; i < alertsArray.length; i++) {
+        const alert = alertsArray[i];
+        const alertName = alert["text"];
+        const total = alert["warn"] + alert["triggered"];
+        const triggered = alert["triggered"];
+        const warn = alert["warn"];
+        const status = alert["last_status"];
+        const url = alert["url"];
+        const prevTs = alert["prev_ts"];
+        const lastTs = alert["last_ts"];
+
+        let dateString = "missing";
+        if (lastTs) {
+            const date = new Date(+lastTs);
+            dateString = date.toLocaleDateString('en-us', dateFormatOptions);
+        } else if (prevTs) {
+            const date = new Date(+prevTs);
+            dateString = "[prev] " + date.toLocaleDateString('en-us', dateFormatOptions);
+        }
 
         const row = document.createElement("tr");
-        row.appendChild(createCell("td", alertName));
+        
+        if (url) {
+            const nameEl = document.createElement("a");
+            nameEl.setAttribute('href', url);
+            nameEl.appendChild(document.createTextNode(alertName));
+            const cell = document.createElement("td");
+            cell.appendChild(nameEl);
+            row.appendChild(cell);
+        } else {
+            row.appendChild(createCell("td", alertName));
+        }
         row.appendChild(createCell("td", total));
         row.appendChild(createCell("td", triggered));
         row.appendChild(createCell("td", warn));
+        row.appendChild(createCell("td", dateString));
         row.appendChild(createCell("td", status));
         row.appendChild(createCell("td", ""));
         const brightness = getRowBrightness(total);
@@ -120,6 +168,8 @@ buildReportButton.onclick = function () {
     footerRow.appendChild(createCell("td", sumTotal));
     footerRow.appendChild(createCell("td", sumTriggered));
     footerRow.appendChild(createCell("td", sumWarn));
+    footerRow.appendChild(createCell("td", ""));
+    footerRow.appendChild(createCell("td", ""));
     footerRow.appendChild(createCell("td", ""));
     newRows.push(footerRow);
     resultsTable.replaceChildren(...newRows);
